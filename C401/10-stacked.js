95e6985751d9ad4884d410b13063028278e4b466
Tim Traylor <ttraylor83@gmail.com>
2022-09-29 16:18:01 -0700

    updated c10

--- C401/10-stacked.js
+++ C401/10-stacked.js
@@ -1,15 +1,54 @@
 class Stack {
-    constructor(){
+    constructor () {
         this.items = [];
         this.count = 0;
     }
 
     push (value) {
-        this.items[this.count] = Element;
+        this.items[this.count] = value;
         this.count += 1
-        console.log(`${Element} added to ${this.count}`)
+        console.log(`${value} added to ${this.count}`)
         return this.count-1
     }
+
+    pop () {
+        if(this.count == 0) return undefined
+        let deleteItem = this.items[this.count-1]
+        this.count -= 1
+        console.log(`${deleteItem} removed`)
+        return deleteItem
+    }
+
+    peek () {
+        console.log(`Top element is ${this.items[this.count-1]}`)
+        return this.items[this.count-1];
+    }
+
+    isEmpty () {
+        console.log(this.county == 0 ? 'Stack is empty' : 'Stack is NOT empty')
+        return this.count == 0
+    }
+
+    size () {
+        console.log(`${this.count} elements are in the stack`)
+        return this.count
+    }
+
+    print () {
+        let str = ''
+        for (let i = 0; i < this.count; i++){
+            str += this.items[i] + ' '
+        }
+        return str
+    }
+
+    clear () {
+        this.items = [];
+        this.count = 0;
+        console.log('Stack is cleared')
+        return this.items
+    }
+
 }
 
 const stack = new Stack();
@@ -18,3 +57,9 @@ stack.push(100);
 stack.push(200);
 stack.push(300);
 
+stack.pop();
+
+stack.push(250);
+stack.peek(2);
+
+console.log(stack.print())
--- C401/class-10.md
+++ C401/class-10.md
@@ -6,4 +6,5 @@
 
 ### Writeup
 
-
+  Stacks and Nodes are like used with Push and Pop to add and removed from top of the stack. We can look at the next one and iterate through the stack like a Linked List. It is basically the same and we can add and remove from the top (or some spot) which is an O(1) action and will require the same computing power regardless of the location in the stack or the length of the queue.
+  
